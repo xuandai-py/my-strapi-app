1fbcf46f5799dc793e4a8de021a7835a00187130
hermitcrab <hermitcrab@pop-os.localdomain>
2022-11-10 00:59:38 +0700

    modify

---
+++ config/env/production/database.js
@@ -0,0 +1,19 @@
+// path: ./config/env/production/database.js
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'xd'),
+      user: env('DATABASE_USERNAME', 'xd'),
+      password: env('DATABASE_PASSWORD', 'xd'),
+      ssl: {
+        ca: env('DATABASE_CA')
+      },
+    },
+    debug: false,
+  },
+});
+
